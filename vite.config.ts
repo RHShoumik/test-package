260f3bff4bb3ec9dcada8a2d4f2bb9c0381291de
RHShoumik <rhshoumik12@gmail.com>
2024-05-07 16:47:25 +0600

    Testing added

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
@@ -9,4 +10,9 @@ export default defineConfig({
       "@/": "/src/",
     },
   },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+  }
 });
