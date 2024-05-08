716cd97978727ba632aae7efb5ccfd5ca1f14f8f
RHShoumik <rhshoumik12@gmail.com>
2024-05-08 13:16:52 +0600

    Add css

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-// import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
-import { peerDependencies } from "./package.json";
+import { peerDependencies } from './package.json'
+import tailwindcss from 'tailwindcss'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
@@ -10,8 +12,12 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './setupTests.ts',
   },
-  // plugins: [react()],
-  plugins: [dts()],
+  plugins: [react(), dts()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   resolve: {
     alias: {
       '@/': '/src/',
